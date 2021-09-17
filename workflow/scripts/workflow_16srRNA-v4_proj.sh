459f93bdf6f42dc939de0e94c7dfa5574b334551
Rene Quevedo <>
2021-09-17 11:06:01 -0400

    Added a longitudinal analysis of taxonomic features vs days

--- workflow/scripts/workflow_16srRNA-v4_proj.sh
+++ workflow/scripts/workflow_16srRNA-v4_proj.sh
@@ -5,10 +5,13 @@ cd ${PROJDIR}
 #############
 #### NEW ####
 # Manifest and Import
+PROJDIR='/cluster/projects/mcgahalab/data/mcgahalab/teresa_metagenomics/16s'
+ID='teresa_metagenomics'
 PROJDIR='/cluster/projects/mcgahalab/data/mcgahalab/PASS/16s'
+ID='PASS'
 cd ${PROJDIR}
 conda activate qiime2-2021.4
-ID='PASS'
+
 
 ## Assembling PE manifest for PHRED33v2
 echo -e "sample-id\tforward-absolute-filepath\treverse-absolute-filepath" \
@@ -27,7 +30,7 @@ for sid in $(cat data/samples.txt); do
   r2=$(ls -d data/fastq/* | grep ${oid}"_R2")
   r1path=$(readlink -f ${r1})
   r2path=$(readlink -f ${r2})
-  
+
   # Format the names of the fastq samples
   echo -e "${nid}\t${r1path}\t${r2path}" >> data/artifacts/pe-manifest
   echo -e "${nid}\t${r1path}" >> data/artifacts/se-manifest-f
@@ -58,10 +61,10 @@ qiime tools import \
 # Sequence QC
 ## Simple visualization of the demultiplexed data
 qcDIR="outputs/pe/seqQC"
-mkdir -p ${qcDir}
+mkdir -p ${qcDIR}
 qiime demux summarize \
-  --i-data data/artifacts/${ID}_demux.qza \
-  --o-visualization ${qcDir}/${ID}_demux.qzv
+  --i-data data/artifacts/${ID}_demux.pe.qza \
+  --o-visualization ${qcDIR}/${ID}_demux.qzv
 
 for direction in f r; do
   qcDIR="outputs/se/${direction}/seqQC"
@@ -80,7 +83,7 @@ qiime dada2 denoise-paired \
   --p-trim-left-r 12 \
   --p-trunc-len-f 150 \
   --p-trunc-len-r 150 \
-  --i-demultiplexed-seqs data/artifacts/${ID}_demux.qza \
+  --i-demultiplexed-seqs data/artifacts/${ID}_demux.pe.qza \
   --o-representative-sequences ${dadaDIR}/${ID}.rep_seq.qza \
   --o-table ${dadaDIR}/${ID}.table.qza \
   --o-denoising-stats ${dadaDIR}/${ID}.stats.qza
@@ -88,13 +91,13 @@ qiime dada2 denoise-paired \
 for direction in f r; do
   dadaDIR="outputs/se/${direction}/features/DADA2"
   mkdir -p ${dadaDIR}
-  
+
   if [[ ${direction} == 'f' ]]; then
     left=18
   else
     left=12
   fi
-  
+
   qiime dada2 denoise-single \
     --p-trim-left ${left} \
     --p-trunc-len 150 \
@@ -108,16 +111,19 @@ done
 ## Visualizing denoising stats
 dadaDIR="outputs/pe/features/DADA2"
 mkdir -p ${dadaDIR}/views
-qiime feature-table summarize \       ### summarize features
+### summarize features
+qiime feature-table summarize \
   --i-table ${dadaDIR}/${ID}.table.qza \
   --o-visualization ${dadaDIR}/views/${ID}.table.qzv \
-  --m-sample-metadata-file data/metadata/sample_metadata-stool.tsv
+  --m-sample-metadata-file data/metadata/sample_metadata.tsv
 
-qiime metadata tabulate \             ### tabulate features
+### tabulate features
+qiime metadata tabulate \
   --m-input-file ${dadaDIR}/${ID}.stats.qza \
   --o-visualization ${dadaDIR}/views/${ID}.stats.qzv
 
-qiime feature-table tabulate-seqs \   ### tabule feature sequences
+### tabule feature sequences
+qiime feature-table tabulate-seqs \
   --i-data ${dadaDIR}/${ID}.rep_seq.qza \
   --o-visualization ${dadaDIR}/views/${ID}.rep-seqs.qzv
 
@@ -127,7 +133,7 @@ for direction in f r; do
   qiime feature-table summarize \
     --i-table ${dadaDIR}/${ID}.table.qza \
     --o-visualization ${dadaDIR}/views/${ID}.table.qzv \
-    --m-sample-metadata-file data/metadata/sample_metadata-stool.tsv
+    --m-sample-metadata-file data/metadata/sample_metadata.tsv
 
   qiime metadata tabulate \
     --m-input-file ${dadaDIR}/${ID}.stats.qza \
@@ -174,7 +180,7 @@ qiime diversity core-metrics-phylogenetic \
   --i-phylogeny ${phyloDIR}/${ID}.rooted-tree.qza \
   --i-table ${dadaDIR}/${ID}.table.qza \
   --p-sampling-depth 5500 \
-  --m-metadata-file data/metadata/sample_metadata-stool.tsv \
+  --m-metadata-file data/metadata/sample_metadata.tsv \
   --output-dir ${abDIR}/${ID}.core-metrics-results
 
 for direction in f r; do
@@ -187,25 +193,25 @@ for direction in f r; do
     --i-phylogeny ${phyloDIR}/${ID}.rooted-tree.qza \
     --i-table ${dadaDIR}/${ID}.table.qza \
     --p-sampling-depth 5500 \
-    --m-metadata-file data/metadata/sample_metadata-stool.tsv \
+    --m-metadata-file data/metadata/sample_metadata.tsv \
     --output-dir ${abDIR}/${ID}.core-metrics-results
-    
+
   ## Visualize Alpha Diversity compared to Groups
   qiime diversity alpha-group-significance \
     --i-alpha-diversity ${abDIR}/${ID}.core-metrics-results/faith_pd_vector.qza \
-    --m-metadata-file data/metadata/sample_metadata-stool.tsv \
+    --m-metadata-file data/metadata/sample_metadata.tsv \
     --o-visualization ${abDIR}/${ID}.core-metrics-results/faith-pd-group-significance.qzv
 
   qiime diversity alpha-group-significance \
     --i-alpha-diversity ${abDIR}/${ID}.core-metrics-results/evenness_vector.qza \
-    --m-metadata-file data/metadata/sample_metadata-stool.tsv \
+    --m-metadata-file data/metadata/sample_metadata.tsv \
     --o-visualization ${abDIR}/${ID}.core-metrics-results/evenness-group-significance.qzv
 
   ## Visualize Beta Diversity comparisons
   qiime diversity beta-group-significance \
     --i-distance-matrix ${abDIR}/${ID}.core-metrics-results/unweighted_unifrac_distance_matrix.qza \
-    --m-metadata-file data/metadata/sample_metadata-stool.tsv \
-    --m-metadata-column disease_status \
+    --m-metadata-file data/metadata/sample_metadata.tsv \
+    --m-metadata-column group-day \
     --o-visualization ${abDIR}/${ID}.core-metrics-results/unweighted-unifrac-body-site-significance.qzv \
     --p-pairwise
 done
@@ -220,7 +226,7 @@ for direction in f r; do
   ## Use a pre-trained Naive Bayes classifier to classify the taxa of each sequence
   classifier_dir='/cluster/projects/mcgahalab/ref/metagenomics/16s_classifiers'
   classifier='silva-138-99-nb-weighted-classifier.qza'
-  
+
   qiime feature-classifier classify-sklearn \
     --i-classifier ${classifier_dir}/${classifier} \
     --i-reads ${dadaDIR}/${ID}.rep_seq.qza \
@@ -230,9 +236,48 @@ for direction in f r; do
   --m-input-file ${taxaDIR}/${ID}.taxonomy.qza \
   --o-visualization ${taxaDIR}/views/${ID}.taxonomy.qzv
 
+  qiime taxa collapse \
+    --i-table ${dadaDIR}/${ID}.table.qza \
+    --i-taxonomy ${taxaDIR}/${ID}.taxonomy.qza \
+    --p-level 6 \
+    --o-collapsed-table ${taxaDIR}/${ID}.lvl6-collapse.qza
+
+  qiime taxa collapse \
+    --i-table ${dadaDIR}/${ID}.table.qza \
+    --i-taxonomy ${taxaDIR}/${ID}.taxonomy.qza \
+    --p-level 7 \
+    --o-collapsed-table ${taxaDIR}/${ID}.lvl7-collapse.qza
+
   qiime taxa barplot \
     --i-table ${dadaDIR}/${ID}.table.qza \
     --i-taxonomy ${taxaDIR}/${ID}.taxonomy.qza \
-    --m-metadata-file data/metadata/sample_metadata-stool.tsv \
+    --m-metadata-file data/metadata/sample_metadata.tsv \
     --o-visualization ${taxaDIR}/views/${ID}.taxa-bar-plots.qzv
+done
+
+# Longitudinal Analysis
+## https://docs.qiime2.org/2021.8/tutorials/longitudinal/?highlight=longitudinal
+for direction in f r; do
+  taxaDIR="outputs/se/${direction}/taxonomy"
+  dadaDIR="outputs/se/${direction}/features/DADA2"
+  longitudinalDIR="outputs/se/${direction}/longitudinal"
+  rmdir ${longitudinalDIR}/lvl6_genus ${longitudinalDIR}/lvl7_species
+
+  qiime longitudinal feature-volatility \
+    --i-table ${taxaDIR}/${ID}.lvl6-collapse.qza \
+    --m-metadata-file data/metadata/sample_metadata.tsv \
+    --p-state-column Day \
+    --p-individual-id-column group-mouse \
+    --p-n-estimators 10 \
+    --p-random-state 17 \
+    --output-dir ${longitudinalDIR}/lvl6_genus
+
+  qiime longitudinal feature-volatility \
+    --i-table ${taxaDIR}/${ID}.lvl7-collapse.qza \
+    --m-metadata-file data/metadata/sample_metadata.tsv \
+    --p-state-column Day \
+    --p-individual-id-column group-mouse \
+    --p-n-estimators 10 \
+    --p-random-state 17 \
+    --output-dir ${longitudinalDIR}/lvl7_species
 done
